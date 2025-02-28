607da0c6f4cefbd8f386aab7847bf5c8096e7844
Mattiias56 <mat.molinas@duocuc.cl>
2025-02-28 17:06:55 -0300

    Agregando eventdos a la galria, y modificando quienes somos

--- src/app/services/galeria.service.ts
+++ src/app/services/galeria.service.ts
@@ -9,8 +9,16 @@ export class GaleriaService {
   constructor() { }
   private videos=[
     {
+      nombre:'¿QUienes Somos?',
+      descripcion:'',
+      link: 'https://drive.google.com/file/d/1Hzv2euPzLEz6TTe7c9d_R8VYsu0Ace1O/preview',
+      fotos:[
+        
+      ]
+    },
+    {
       nombre:'Agacech 2024',
-      descripcion:'entrevista a mariana alejandra letelier en Agacech ',
+      descripcion:'Entrevista a Maria Alejandra Letelier - Safincorp ',
       link: 'https://www.youtube.com/embed/tDm2caLU44w?si=08no9IZdh_wEebHf',
       fotos:[
         
@@ -26,7 +34,52 @@ export class GaleriaService {
         'assets/img/fotos-eventos/agacech/cigsa3.webp',
         'assets/img/fotos-eventos/agacech/cigsa4.webp',        
       ]
-    }
+    },
+    {
+        nombre:'Certificados de Participación',
+        descripcion:'',
+        link: '',
+        fotos:[
+          'assets/img/fotos-eventos/capacitacion/certificado-paarticipacion.webp',
+          'assets/img/fotos-eventos/capacitacion/certificado-sofofa.webp',                
+          'assets/img/fotos-eventos/capacitacion/certificado-cigsa.webp'                
+        ]
+    },
+    
+  {
+    nombre:'Evento BCI',
+    descripcion:'',
+    link: '',
+    fotos:[
+      'assets/img/fotos-eventos/bci/evento-bci-1.webp',
+      'assets/img/fotos-eventos/bci/evento-bci-2.webp',
+      'assets/img/fotos-eventos/bci/evento-bci-3.webp',
+      'assets/img/fotos-eventos/bci/evento-bci-4.webp'        
+    ]
+  },
+  {
+    nombre:'Segundo Evento AGACECH',
+    descripcion:'',
+    link: '',
+    fotos:[
+      'assets/img/fotos-eventos/agacech-2/segundo-evento-agcech-1.webp',
+      'assets/img/fotos-eventos/agacech-2/segundo-evento-agcech-2.webp',
+      'assets/img/fotos-eventos/agacech-2/segundo-evento-agcech-3.webp',
+      'assets/img/fotos-eventos/agacech-2/segundo-evento-agcech-4.webp'        
+    ]
+  },
+  {
+    nombre:'Convención ANS 2023',
+    descripcion:'',
+    link: '',
+    fotos:[
+      'assets/img/fotos-eventos/ans/convencion-ans.webp',
+      'assets/img/fotos-eventos/ans/convencion-ans2.webp',
+      'assets/img/fotos-eventos/ans/convencion-ans3.webp',
+      'assets/img/fotos-eventos/ans/convencion-ans4.webp'        
+    ]
+  }
+  
 
   ]
 
