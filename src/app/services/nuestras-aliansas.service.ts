590bf4d509cc1fe687b5d8924dbe700e9dbe6c41
Carolina Jesús Cancino Tonacca <carolinajc.tonacca@gmail.com>
2025-12-02 16:56:04 -0300

    feat: eliminar alianza armis

--- src/app/services/nuestras-aliansas.service.ts
+++ src/app/services/nuestras-aliansas.service.ts
@@ -18,7 +18,6 @@ export class NuestrasAliansasService {
     const alianzas: Alianza[] = [
       {  nombre: 'Vivo en Condominio Consultores', logoUrl: 'assets/img/aliansas/vivoencondominio.webp'  },
       {  nombre: 'Balloqui Ingenieria SPA', logoUrl: 'assets/img/aliansas/LOGO-BALLOQUI.webp'},
-      {  nombre: 'Estudio Juridico ARMIS', logoUrl: 'assets/img/aliansas/armis.webp' },
       {  nombre: 'Asesoria en Prevención  de Riesgos APES', logoUrl: 'assets/img/aliansas/APES.webp' },
       {  nombre: 'SV Qualitas Consultores', logoUrl: 'assets/img/aliansas/svCualitas.webp'},
       {  nombre: 'Cap-Instalmet Constructores', logoUrl: 'assets/img/aliansas/cap-instment.webp' },
